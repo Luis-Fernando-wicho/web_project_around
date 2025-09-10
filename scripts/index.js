965871849ea6e5f04420c713064f5b654c60e563
luisfernando <luisfernandomsmg@gmail.com>
2025-09-10 12:51:17 -0600

    Check point 6 targetas agregadas desde html

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,38 @@
 /* boton para editar */
 const VentanaFlotante = document.querySelector(".popup");
+const VentanaFlotantePlaces = document.querySelector(".popupPlaces");
 
 const InfoEdit = document.querySelector(".profile__info_edit");
+const PlacesEdit = document.querySelector(".profile__add-button");
 const ButtonX = document.querySelector(".popup__x");
+const buttonXPlaces = document.querySelector(".popupPlaces__x");
 
 let NameProfile = document.querySelector(".profile__info_name");
 let OcupationProfile = document.querySelector(".profile__info_ocupation");
 
-let NewNameProfile = document.querySelector(".form__label_name");
-let NewOcupationProfile = document.querySelector(".form__label_ocupation");
+const NewNameProfile = document.querySelector(".form__label_name");
+const NewOcupationProfile = document.querySelector(".form__label_ocupation");
 
 let formForm = document.querySelector(".form");
 
-let likebutton = document.querySelector(".element__white_button");
+const likebutton = document.querySelectorAll(".element__white_button");
 
 const SaveButton = document.querySelector(".form__submit");
-
+const saveButtonPlaces = document.querySelector(".formPlaces__submit");
+////////////////////////////////////////////////////////////////////
 /* mostrar ventana flotante */
 function ProfileEdit() {
   VentanaFlotante.setAttribute("style", "display: grid;");
 }
 InfoEdit.addEventListener("click", ProfileEdit);
+
+/* mostrar ventana flotante places*/
+
+function placeEdit() {
+  VentanaFlotantePlaces.setAttribute("style", "display: grid;");
+}
+PlacesEdit.addEventListener("click", placeEdit);
+////////////////////////////////////////////////////////////////////////////
 
 /* cerrar ventana flotante */
 
@@ -29,7 +41,18 @@ function CerrarVentana() {
 }
 ButtonX.addEventListener("click", CerrarVentana);
 
-SaveButton.addEventListener("click", CerrarVentana);
+saveButtonPlaces.addEventListener("click", CerrarVentana);
+
+/* cerrar ventana flotante Places */
+
+function cerrarVentanaPlaces() {
+  VentanaFlotantePlaces.removeAttribute("style", "display: grid;");
+}
+buttonXPlaces.addEventListener("click", cerrarVentanaPlaces);
+
+SaveButton.addEventListener("click", cerrarVentanaPlaces);
+
+///////////////////////////////////////////////////////////////////////////////////
 
 /* CAMBIAR NOMBRE */
 
@@ -42,12 +65,25 @@ function CambiarNombre(event) /* no sube datos */ {
   NewOcupationProfile.value = "";
 }
 formForm.addEventListener("submit", CambiarNombre);
+formForm.addEventListener("submit", CerrarVentana);
+
+///////////////////////////////////////////////////////////////////////////////////////
 
 /* like button */
-function Like() {
+/* function Like() {
   likebutton.setAttribute("style", "background-image: url(/images/Union.svg);");
 }
-likebutton.addEventListener("click", Like);
+likebutton.addEventListener("click", Like); */
+
+/* likebutton.addEventListener("click", function (evt) {
+  evt.target.classList.toggle(".element__white_button-active");
+}); */
+
+likebutton.addEventListener("click", function (evt) {
+  evt.target.classList.toggle(".element__white_button-active");
+});
+
+/////////////////////////////////////////////////////////////////////////////////////
 
 /* desabilitar botton "trabajando" */
 
@@ -57,9 +93,16 @@ function disabledButton() {
     NewOcupationProfile.value.length === 0
   ) {
     SaveButton.setAttribute("disabled", true);
-    SaveButton.setAttribute("style", "background-color: grey;");
+    SaveButton.classList.add("form__submit_disable");
   } else {
     SaveButton.removeAttribute("disabled");
-    SaveButton.setAttribute("style", "background-color: black;");
+    SaveButton.classList.add("form__submit");
   }
 }
+
+function disabledButton() {
+  SaveButton.setAttribute("disabled", true);
+  SaveButton.classList.add("form__submit_disable");
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
